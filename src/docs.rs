46d0e66a0bad6d4711b703746454c043b4df5f69
Divyanshu Prakash <divyanshuprakash914@gmail.com>
2026-06-14 20:31:20 +0530

    added

--- src/docs.rs
+++ src/docs.rs
@@ -4,14 +4,14 @@ use crate::models::backend_plan::{BackendPlanItem, BackendPlanResponse};
 use crate::models::blueprint::{ApiRouteBlueprint, BlueprintResponse, PageBlueprint};
 use crate::models::file_plan::{FilePlanItem, FrontendPlanResponse};
 use crate::models::generated_code::{CodePreviewResponse, GeneratedFile};
-use crate::models::generated_project::GeneratedProjectResponse;
 use crate::models::intent::{IntentRequest, IntentResponse};
 use crate::models::project_plan::ProjectPlanResponse;
+use crate::models::generated_project::GeneratedProjectResponse;
 
 use crate::routes::ai::{
     __path_detect_intent, __path_generate_backend_plan, __path_generate_blueprint,
-    __path_generate_code_preview, __path_generate_frontend_plan, __path_generate_project,
-    __path_generate_project_plan,
+    __path_generate_code_preview, __path_generate_frontend_plan, __path_generate_project_plan,
+    __path_generate_project,
 };
 use crate::routes::health::__path_health_check;
 
